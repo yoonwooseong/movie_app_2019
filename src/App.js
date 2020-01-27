753edb40bae4c69e93e8c558475451851c3d3716
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-01-27 11:52:23 +0900

    potato

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() {
-  return <div><h1>Hello!!!!!!</h1></div>;
+  return <div>
+    <h1>Hello!!!!!!</h1>
+    <Potato />
+  </div>;
+  
 }
 
 export default App;
