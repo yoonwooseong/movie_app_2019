c72ca8df0aa2df9f4ddde534f99d275260aa40a7
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-02-01 12:58:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+  }
 }
+
 
 export default App;
