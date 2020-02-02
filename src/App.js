285d4f95a0ffb0addfa07d5581a12ff3ad0a6842
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-02-02 19:24:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 
 class App extends React.Component{
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1}));
+    isLoading: true,
+    movies: []
   };
   componentDidMount() {
-    console.log("Component redered");
-  };
-  componentDidUpdate() {
-    console.log("I just updated");
-  };
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
-  };
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 6000)
+  }
   render() {
-    console.log("I'm rendering");
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
   }
 }
-
 
 export default App;
