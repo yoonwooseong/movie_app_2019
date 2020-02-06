4e1fa60dba656897185e60d6bb332b512f78a247
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-02-06 19:50:20 +0900

    update!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component{
   state = {
@@ -10,7 +11,7 @@ class App extends React.Component{
   getMovies = async () => {
     const {
       data: { 
-        data : {movies}
+        data : { movies }
     }
   } = await axios.get(
       "https://yts-proxy.now.sh/list_movies.json?sort_by_rating"
@@ -23,13 +24,13 @@ class App extends React.Component{
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class = "container">
+      <section className = "container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
               <Movie
                 key ={movie.id} 
@@ -38,6 +39,7 @@ class App extends React.Component{
                 title={movie.title} 
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
             />
           ))}
         </div>
