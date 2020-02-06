6f45a5598dd675eaabea06f5f5424035ac0243a3
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-02-07 08:20:42 +0900

    Update Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -15,7 +15,7 @@ function Movie({ year, title, summary, poster, genres}) {
             <li key={index} className="genres__genre">{genre}</li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0,150)}...</p>
       </div>
     </div>
   );
