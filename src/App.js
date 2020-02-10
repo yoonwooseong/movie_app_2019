ff542498593917cc5536f43498b321921f1f668d
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-02-10 20:30:58 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Navigation from './components/Navigation';
 
 function App() {
-  return <HashRouter>
-    <Route path="/" exact={true} component={Home}/>
-    <Route path="/about" component={About}/>
-  </HashRouter>
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+    </HashRouter>
+  );
 }
 
 export default App;
