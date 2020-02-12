ed35bfaa80ceee2d3725a8b972c5b72734bee975
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-02-12 14:19:02 +0900

    Update Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-function Detail(props){
-    console.log(props);
-    return <span>hello</span>;
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if(location.state === undefined) {
+            history.push("/");
+        }
+    }
+
+    render() {
+        return <span>hello</span>;
+    }
 }
 
 export default Detail;
