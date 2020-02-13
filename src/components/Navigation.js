7a8bd14139f3720e74459effc06da5f12b53c102
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-02-13 11:26:43 +0900

    css update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 function Navigation() {
     return (
-     <div>
+     <div className="nav">
         <Link to="/">Home</Link>
         <Link to="About">About</Link>
     </div>
